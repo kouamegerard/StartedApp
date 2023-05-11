637cb6d50926084bbbc6f61e6941a78555e9e4b8
KOUAME GERARD KODJO <gerardkouame8@gmail.com>
2023-05-11 21:52:29 +0000

    Categories view and components finished

--- src/components/categories/CategoriesCardComponent.js
+++ src/components/categories/CategoriesCardComponent.js
@@ -1,12 +1,48 @@
-import { View, Text } from 'react-native'
+import { View, Text, Pressable, Image, StyleSheet } from 'react-native'
 import React from 'react'
+import { COLORS } from '../../utils/constants/theme'
 
-const CategoriesCardComponent = () => {
+const CategoriesCardComponent = ({ item, index }) => {
   return (
-    <View>
-      <Text>CategoriesCardComponent</Text>
-    </View>
+    <Pressable 
+      onPress={ () => console.warn(item.name) }
+      key={index}
+      style={styles.itemContainer}>
+      <Image 
+        source={ item.image } 
+        style={styles.image}
+      />
+      <Text style={{ color: "#FFFFFF" }}>{ item.name }</Text>
+    </Pressable>
   )
 }
 
 export default CategoriesCardComponent
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 12,
+  },
+    itemContainer: {
+      width: "31%",
+      flexDirection: "row",
+      flexWrap: "wrap",
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 8,
+      borderColor: COLORS.main,
+      backgroundColor: COLORS.primary,
+      borderRadius: 8,
+      borderWidth: 1,
+      marginHorizontal: 4,
+      marginVertical: 4,
+    },
+    image: { 
+      width: "100%", 
+      aspectRatio: 1, 
+      height: '100%',
+    }
+  });
--- src/screens/Categories.js
+++ src/screens/Categories.js
@@ -1,25 +1,13 @@
-import { View, Text, FlatList, Pressable, StyleSheet, Image } from 'react-native'
+import { View, FlatList, StyleSheet, } from 'react-native'
 import React from 'react'
 import TopHeaderComponent from '../components/headers/TopHeaderComponent'
 import categoriesData from '../data/Categories/categoriesData'
 import { COLORS } from '../utils/constants/theme'
+import CategoriesCardComponent from '../components/categories/CategoriesCardComponent'
 
 const Categories = () => {
 
   const categories = categoriesData;
-
-  const cardItem = ({ item, index }) => {
-    return (
-      <Pressable 
-        onPress={ () => console.warn(item.name) }
-        style={styles.itemContainer}>
-        <Image 
-          source={ item.image } 
-          style={styles.image}
-        />
-      </Pressable>
-    )
-  }
 
   return (
     <View 
@@ -32,11 +20,11 @@ const Categories = () => {
       <TopHeaderComponent />
       <View style={styles.container}>
         <FlatList 
-            data={categories}
-            renderItem={ cardItem }
-            numColumns={4}
-            showsVerticalScrollIndicator={false}
-            style={{ width: "100%", height: "100%", }}
+          data={categories}
+          renderItem={ ({ item, index }) => <CategoriesCardComponent item={item} index={index} /> }
+          numColumns={3}
+          showsVerticalScrollIndicator={false}
+          style={{ width: "100%", height: "100%", }}
         />
       </View>
     </View>
@@ -52,23 +40,5 @@ container: {
   alignItems: 'center',
   justifyContent: 'center',
   padding: 12,
-  backgroundColor: "#cecece"
 },
-  itemContainer: {
-    width: "15%",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    padding: 8,
-    borderColor: COLORS.main,
-    backgroundColor: COLORS.primary,
-    borderRadius: 8,
-    borderWidth: 1,
-    marginHorizontal: 4,
-    marginVertical: 4,
-  },
-  image: { 
-    width: "100%", 
-    aspectRatio: 1, 
-    height: '100%',
-  }
 });
